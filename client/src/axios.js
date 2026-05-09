53a2d183b6ae657a0c3c10edab392952ad4f5901
rithi-ka-v <rithikalai927@gmail.com>
2026-05-09 15:01:25 +0530

    add the chnages

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mern-ecommerce-1-jug0.onrender.com",
-  withCredentials: true   // 🔥 GLOBAL FIX
+  baseURL: "https://mern-ecommerce-2-tcjx.onrender.com",
+  withCredentials: true,
 });
 
 export default api;

e42011e2660f81d5570c31ee3bcd9af971dc9bbe
rithi-ka-v <rithikalai927@gmail.com>
2026-05-09 14:30:00 +0530

    add the changes

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mern-ecommerce-il1t.onrender.com",
+  baseURL: "https://mern-ecommerce-1-jug0.onrender.com",
   withCredentials: true   // 🔥 GLOBAL FIX
 });
 
